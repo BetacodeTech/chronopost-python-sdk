132c4e196939487d95d7c84be9982b41fae3a27c
Andre <rosado_andre@live.com.pt>
2019-12-09 15:50:00 +0000

    successful request

--- chronopost_python_sdk/Delivery.py
+++ chronopost_python_sdk/Delivery.py
@@ -1,21 +1,19 @@
-import datetime
-
 
 class Delivery:
     def __init__(self, OrigemMoradaNome: str, OrigemMoradaLinha1: str, OrigemMoradaCodigoPostal: str, OrigemMoradaLocalidade: str,
                  OrigemEmail: str, DestinoMoradaNome: str, DestinoMoradaLinha1: str,
-                 NumeroVolumes: int, DataExpedicao: datetime,
+                 NumeroVolumes: int, DataExpedicao: str,
                  Conta: str = "02496101", OrigemMoradaPais: str = "PT", TipoDestino: int = 1,
                  EnviarEtiquetasEmail: int = 2,
-                 DestinoMoradaCodigoPostal: str = None, DestinoMoradaLocalidade: str = None, DestinoMoradaPais: str="PT",
-                 OrigemMoradaLinha2=None, OrigemTelefone=None, OrigemTelemovel=None,
-                 OrigemFax=None, OrigemContactoNome=None, OrigemContactoTelefone=None,
-                 OrigemContactoTelemovel=None, OrigemContactoEmail=None, DestinoLojaId=None,
-                 DestinoMoradaLinha2=None, DestinoContactoTelefone=None, DestinoContactoTelemovel=None,
-                 DestinoContactoNome=None,
-                 DestinoContactoEmail=None, DestinoLojaAlternativaId=None, DestinoLojaAlternativaNomeReceptor=None,
-                 DestinoLojaAlternativaEmailReceptor=None, DestinoLojaAlternativaTelemovelReceptor=None, ObservacoesLinha1=None,
-                 ObservacoesLinha2=None, Peso=None, Referencia=None, ValorCOD=None):
+                 DestinoMoradaCodigoPostal: str = "", DestinoMoradaLocalidade: str = "", DestinoMoradaPais: str="PT",
+                 OrigemMoradaLinha2="", OrigemTelefone="", OrigemTelemovel="",
+                 OrigemFax="", OrigemContactoNome="", OrigemContactoTelefone="",
+                 OrigemContactoTelemovel="", OrigemContactoEmail="", DestinoLojaId="",
+                 DestinoMoradaLinha2="", DestinoContactoTelefone="", DestinoContactoTelemovel="",
+                 DestinoContactoNome="",
+                 DestinoContactoEmail="", DestinoLojaAlternativaId="", DestinoLojaAlternativaNomeReceptor="",
+                 DestinoLojaAlternativaEmailReceptor="", DestinoLojaAlternativaTelemovelReceptor="", ObservacoesLinha1="",
+                 ObservacoesLinha2="", Peso="", Referencia="", ValorCOD=""):
         self.Conta = Conta
         self.OrigemMoradaNome = OrigemMoradaNome
         self.OrigemMoradaLinha1 = OrigemMoradaLinha1
@@ -34,30 +32,23 @@ class Delivery:
         self.DestinoMoradaCodigoPostal = DestinoMoradaCodigoPostal
         self.DestinoMoradaLocalidade = DestinoMoradaLocalidade
         self.DestinoMoradaPais = DestinoMoradaPais
-        self.OrigemMoradaLinha2 = OrigemMoradaLinha2
-        self.OrigemTelefone = OrigemTelefone
-        self.OrigemTelemovel = OrigemTelemovel
-        self.OrigemFax = OrigemFax
-        self.OrigemContactoNome = OrigemContactoNome
-        self.OrigemContactoTelefone = OrigemContactoTelefone
-        self.OrigemContactoTelemovel = OrigemContactoTelemovel
-        self.OrigemContactoEmail = OrigemContactoEmail
-        self.DestinoLojaId = DestinoLojaId
-        self.DestinoMoradaLinha2 = DestinoMoradaLinha2
-        self.DestinoContactoTelefone = DestinoContactoTelefone
-        self.DestinoContactoTelemovel = DestinoContactoTelemovel
+        # self.OrigemMoradaLinha2 = OrigemMoradaLinha2
+        # self.OrigemTelefone = OrigemTelefone
+        # self.OrigemTelemovel = OrigemTelemovel
+        # self.OrigemFax = OrigemFax
+        # self.OrigemContactoNome = OrigemContactoNome
+        # self.OrigemContactoTelefone = OrigemContactoTelefone
+        # self.OrigemContactoTelemovel = OrigemContactoTelemovel
+        # self.OrigemContactoEmail = OrigemContactoEmail
+        # self.DestinoLojaId = DestinoLojaId
+        # self.DestinoMoradaLinha2 = DestinoMoradaLinha2
+        # self.DestinoContactoTelefone = DestinoContactoTelefone
+        # self.DestinoContactoTelemovel = DestinoContactoTelemovel
         #self.DestinoMoradaFax = DestinoMoradaFax
         #self.DestinoMoradaEmail = DestinoMoradaEmail
-        self.DestinoContactoNome = DestinoContactoNome
+        #self.DestinoContactoNome = DestinoContactoNome
         #self.DestinoMoradaTelefone = DestinoMoradaTelefone
         #self.DestinoMoradaTelemovel = DestinoMoradaTelemovel
-        self.DestinoContactoEmail = DestinoContactoEmail
-        self.DestinoLojaAlternativaId = DestinoLojaAlternativaId
-        self.DestinoLojaAlternativaNomeReceptor = DestinoLojaAlternativaNomeReceptor
-        self.DestinoLojaAlternativaEmailReceptor = DestinoLojaAlternativaEmailReceptor
-        self.DestinoLojaAlternativaTelemovelReceptor = DestinoLojaAlternativaTelemovelReceptor
-        self.ObservacoesLinha1 = ObservacoesLinha1
-        self.ObservacoesLinha2 = ObservacoesLinha2
-        self.Peso = Peso
-        self.Referencia = Referencia
-        self.ValorCOD = ValorCOD
+        #self.Peso = Peso
+        #self.Referencia = Referencia
+        #self.ValorCOD = ValorCOD
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime
+import requests
 
 from chronopost_python_sdk import Delivery
 from chronopost_python_sdk.ChronopostService import ChronopostService
@@ -13,14 +14,12 @@ class ChronopostTest(unittest.TestCase):
         pass
 
     def testGetResult(self):
-        today = datetime.datetime(2019, 12, 4)
+        today = datetime.datetime(2029, 12, 4)
 
         delivery = Delivery(OrigemMoradaNome='rafik', OrigemMoradaLinha1='Rua xyz', OrigemMoradaCodigoPostal='1234-123',
                             OrigemMoradaLocalidade='Lisboa', OrigemEmail='sloth577@gmail.com',
                             DestinoMoradaNome='andre', DestinoMoradaCodigoPostal='7080-038', DestinoMoradaLocalidade='Vendas Novas',
                             DestinoMoradaLinha1='Rua JFF', NumeroVolumes=1, DataExpedicao=today)
-
-        result = self.chronopost_service.dispatch(delivery)
 
         self.assertIsNotNone(delivery)
         self.assertEqual(delivery.Conta, '02496101')
@@ -40,3 +39,59 @@ class ChronopostTest(unittest.TestCase):
         self.assertEqual(delivery.EnviarEtiquetasEmail, 2)
         self.assertEqual(delivery.NumeroVolumes, 1)
         self.assertEqual(delivery.DataExpedicao, today)
+
+        response_json = self.chronopost_service.dispatch(delivery)
+
+        self.assertIn('Codigo', response_json)
+        self.assertIn('Descricao', response_json)
+        self.assertIn('NrGuia', response_json)
+        self.assertIn('CorpoResposta', response_json)
+
+    def testRequest(self):
+        url = 'http://cliente.chronopost.pt:10002/Services/Services.asmx'
+        header = {'Content-type': 'text/xml'}
+
+        today = datetime.datetime(2019, 12, 4)
+
+        delivery = Delivery(OrigemMoradaNome='rafik', OrigemMoradaLinha1='Rua xyz', OrigemMoradaCodigoPostal='1234-123',
+                            OrigemMoradaLocalidade='Lisboa', OrigemEmail='sloth577@gmail.com',
+                            DestinoMoradaNome='andre', DestinoMoradaCodigoPostal='7080-038',
+                            DestinoMoradaLocalidade='Vendas Novas',
+                            DestinoMoradaLinha1='Rua JFF', NumeroVolumes=1, DataExpedicao=today)
+
+        text = '''
+<soap-env:Envelope
+    xmlns:soap-env="http://www.w3.org/2003/05/soap-envelope">
+    <soap-env:Body>
+        <ns0:RegistarExpedicao3
+            xmlns:ns0="http://tempuri.org/">
+            <ns0:username>024961</ns0:username>
+            <ns0:password>X024961X</ns0:password>
+            <ns0:expedicao>
+                <ns0:Conta>02496101</ns0:Conta>
+                <ns0:OrigemMoradaNome>rafik</ns0:OrigemMoradaNome>
+                <ns0:OrigemMoradaLinha1>Rua xyz</ns0:OrigemMoradaLinha1>
+                <ns0:OrigemMoradaCodigoPostal>1234-123</ns0:OrigemMoradaCodigoPostal>
+                <ns0:OrigemMoradaLocalidade>Lisboa</ns0:OrigemMoradaLocalidade>
+                <ns0:OrigemMoradaPais>PT</ns0:OrigemMoradaPais>
+                <ns0:OrigemEmail>sloth577@gmail.com</ns0:OrigemEmail>
+                <ns0:TipoDestino>{destino}</ns0:TipoDestino>
+                <ns0:DestinoMoradaNome>andre</ns0:DestinoMoradaNome>
+                <ns0:DestinoMoradaLinha1>Rua JFF</ns0:DestinoMoradaLinha1>
+                <ns0:DestinoMoradaCodigoPostal>7080-038</ns0:DestinoMoradaCodigoPostal>
+                <ns0:DestinoMoradaLocalidade>Vendas Novas</ns0:DestinoMoradaLocalidade>
+                <ns0:DestinoMoradaPais>PT</ns0:DestinoMoradaPais>
+                <ns0:DataExpedicao>2020-12-04 00:00:00</ns0:DataExpedicao>
+                <ns0:EnviarEtiquetasEmail>2</ns0:EnviarEtiquetasEmail>
+                <ns0:NumeroVolumes>1</ns0:NumeroVolumes>
+            </ns0:expedicao>
+            <ns0:tipoResposta>1</ns0:tipoResposta>
+            <ns0:enviarEmail>1</ns0:enviarEmail>
+            <ns0:email>rosado_andre@live.com.pt</ns0:email>
+            <ns0:tipoEtiqueta>0</ns0:tipoEtiqueta>
+        </ns0:RegistarExpedicao3>
+    </soap-env:Body>
+</soap-env:Envelope>      
+        '''
+
+        r = requests.post(url, data=text, headers=header)
