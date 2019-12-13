d7e7eab4d591ad513508a0bea3dcdb226b4e58e4
Marco Mendão <mac.mendao@gmail.com>
2019-12-13 17:08:36 +0000

    fixes

--- chronopost_python_sdk/Delivery.py
+++ chronopost_python_sdk/Delivery.py
@@ -1,29 +1,35 @@
+import logging
+
 
 class Delivery:
-    def __init__(self, OrigemMoradaNome: str, OrigemMoradaLinha1: str, OrigemMoradaCodigoPostal: str, OrigemMoradaLocalidade: str,
-                 OrigemEmail: str, DestinoMoradaNome: str, DestinoMoradaLinha1: str,
+    def __init__(self, OrigemMoradaNome: str, OrigemMorada: str, OrigemMoradaCodigoPostal: str, OrigemMoradaLocalidade: str,
+                 OrigemEmail: str, DestinoMoradaNome: str, DestinoMorada: str,
                  NumeroVolumes: int, DataExpedicao: str,
                  Conta: str = "02496101", OrigemMoradaPais: str = "PT", TipoDestino: int = 1,
                  EnviarEtiquetasEmail: int = 2,
                  DestinoMoradaCodigoPostal: str = "", DestinoMoradaLocalidade: str = "", DestinoMoradaPais: str="PT",
                  OrigemMoradaLinha2="", OrigemTelefone="", OrigemTelemovel="",
                  OrigemFax="", OrigemContactoNome="", OrigemContactoTelefone="",
-                 OrigemContactoTelemovel="", OrigemContactoEmail="", DestinoLojaId="",
-                 DestinoMoradaLinha2="", DestinoContactoTelefone="", DestinoContactoTelemovel="",
+                 OrigemContactoTelemovel="", OrigemContactoEmail="", DestinoLojaId="", DestinoContactoTelefone="", DestinoContactoTelemovel="",
                  DestinoContactoNome="",
                  DestinoContactoEmail="", DestinoLojaAlternativaId="", DestinoLojaAlternativaNomeReceptor="",
                  DestinoLojaAlternativaEmailReceptor="", DestinoLojaAlternativaTelemovelReceptor="", ObservacoesLinha1="",
                  ObservacoesLinha2="", Peso="", Referencia="", ValorCOD=""):
+
+        self.DestinoMoradaLinha1 = DestinoMorada[0:31]
+        self.DestinoMoradaLinha2 = DestinoMorada[31:64]
         self.Conta = Conta
         self.OrigemMoradaNome = OrigemMoradaNome
-        self.OrigemMoradaLinha1 = OrigemMoradaLinha1
+
+        self.OrigemMoradaLinha1 = OrigemMorada[0:31]
+        self.OrigemMoradaLinha2 = OrigemMorada[31:63]
+
         self.OrigemMoradaCodigoPostal = OrigemMoradaCodigoPostal
         self.OrigemMoradaLocalidade = OrigemMoradaLocalidade
         self.OrigemMoradaPais = OrigemMoradaPais
         self.OrigemEmail = OrigemEmail
         self.TipoDestino = TipoDestino
         self.DestinoMoradaNome = DestinoMoradaNome
-        self.DestinoMoradaLinha1 = DestinoMoradaLinha1
         self.NumeroVolumes = NumeroVolumes
         #self.tipoResposta = tipoResposta
         self.DataExpedicao = DataExpedicao
@@ -52,3 +58,9 @@ class Delivery:
         #self.Peso = Peso
         #self.Referencia = Referencia
         #self.ValorCOD = ValorCOD
+
+        if self.OrigemMoradaLinha1 + self.OrigemMoradaLinha2 != OrigemMorada:
+            logging.warning("Origem Morada has more than max 64 characters allowed please consider limiting this field")
+
+        if self.DestinoMoradaLinha1 + self.DestinoMoradaLinha2 != DestinoMorada:
+            logging.warning("Destino Morada has more than max 64 characters allowed please consider limiting this field")
